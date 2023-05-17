ca6f4e3478ca75f03b54ece9b3a36977bba9c272
Dominik Pierdiluca <wi22b124@technikum-wien.at>
2023-05-17 10:46:46 +0200

    corrected annotations

--- src/main/java/com/example/maintenancemonitor/SumController.java
+++ src/main/java/com/example/maintenancemonitor/SumController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 public class SumController {
 
-    @RequestMapping("/api/sum");
+    @RequestMapping("/api/sum")
     public int sum(
                                     @RequestParam int a,
                                     @RequestParam int b){
