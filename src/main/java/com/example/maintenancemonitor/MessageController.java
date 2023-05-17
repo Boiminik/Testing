06cd9cee202e8915c38a1fe932ea0fc46cf0c7e5
Dominik Pierdiluca <wi22b124@technikum-wien.at>
2023-05-17 10:49:21 +0200

    corrected annotations

--- src/main/java/com/example/maintenancemonitor/MessageController.java
+++ src/main/java/com/example/maintenancemonitor/MessageController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.*;
 public class MessageController {
     String ogmessage = "Everything operates as expected";
     String message = ogmessage;
-    @RequestMapping("/api/message");
+    @RequestMapping("/api/message")
     public String showMessage(){
         return message;
     }
-    @RequestMapping("/api/message/set");
+    @RequestMapping("/api/message/set")
     public String setMessage(@RequestParam String custommessage){
         message = custommessage;
         return "ok";
     }
-    @RequestMapping("/api/message/reset");
+    @RequestMapping("/api/message/reset")
     public String resetMessage(){
         message = ogmessage;
         return "ok";
